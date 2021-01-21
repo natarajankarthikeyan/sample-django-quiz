3438862887261768ed2fb5907ed2830d7e45ae10
natarajankarthikeyan <nkarthikeyan113@gmail.com>
2021-01-21 07:02:09 -0800

    firstcommit

--- myquiz/settings.py
+++ myquiz/settings.py
@@ -15,7 +15,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -25,7 +31,7 @@ SECRET_KEY = '3nh0t8dlr981qf8@jpbs+5m$%ituuv6mz&s&dornnka*qz2ao$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','django-myquiz.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','django-myquiz.herokuapp.com'  ]
 
 
 # Application definition
